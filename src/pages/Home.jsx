2a044ef3c90f59698a186272e18191075acda1cf
CodeZero <codexzero.tech@gmail.com>
2025-11-24 03:26:02 -0600

    Add laboratory and articles sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import About from "./../components/sections/About";
 import Skills from "./../components/sections/Skills";
 import Projects from "./../components/sections/Projects";
 import Experience from "./../components/sections/Experience";
+import Laboratory from "./../components/sections/Laboratory";
+import Articles from "./../components/sections/Articles";
 import Contact from "./../components/sections/Contact";
 import Footer from "./../components/layout/Footer";
 import ScrollProgress from "./../components/ui/ScrollProgress";
@@ -19,6 +21,8 @@ function Home() {
         <Skills />
         <Projects />
         <Experience />
+        <Laboratory />
+        <Articles />
         <Contact />
       </main>
       <Footer />
